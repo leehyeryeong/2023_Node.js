d59b01e209b244d4ed4d75918d62be282322cb8e
leehyeryeong <w2108@e-mirim.hs.kr>
2023-03-15 10:03:03 +0900

    arrow function body에 return 값 하나일 때

--- 문법.js
+++ 문법.js
@@ -6,4 +6,25 @@
 //상수
 const a = 30;
 console.log(a); //30
-a = 10;         //에러: const는 값을 바꿀 수 없다
+//a = 10;
+//윗 줄을 주석 처리 안 하고 실행시 Error: const는 값을 바꿀 수 없다
+
+//함수
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+result = add(1, 2);
+console.log(result); //3
+
+plus = add;          //plus도 add()의 역할을 할 수 있음
+result = plus(3, 4);
+console.log(result);
+
+// sub = function(n1, n2) {
+//   return n1 - n2;
+// }
+sub = (n1, n2) => n1 - n2;
+
+result = sub(3, 2);
+console.log(result);  //1
