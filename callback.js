6c3decf1d8e3db1df2f178105a72869034ae9687
leehyeryeong <w2108@e-mirim.hs.kr>
2023-03-16 15:02:06 +0900

    callback 1

---
+++ callback.js
@@ -0,0 +1,13 @@
+function somefunc(callback) {
+  console.log("somefunc 실행");
+
+  //somefunc의 기능을 수행하고 난 후에
+  //callback()이 실행된다.
+  callback();
+}
+
+function cb() {
+  console.log("callback 함수 실행");
+}
+
+somefunc(cb);
