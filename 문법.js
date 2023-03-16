f921e5fc13057b084b21997ba9cf5c2a6948b146
leehyeryeong <w2108@e-mirim.hs.kr>
2023-03-16 14:03:54 +0900

    object

--- 문법.js
+++ 문법.js
@@ -33,3 +33,24 @@ square = (num) => num * num;
 
 result = square(3);
 console.log(result);
+
+const cathy = {
+  "name": "cathy",
+  "age": 19,
+  "skills": ["자바스크립트", "파이썬", "코볼"]
+};
+
+//{name: 'cathy', age: 19, skills: ['자바스크립트', '파이썬', '코볼']}
+console.log(cathy);
+
+//cathy
+console.log(cathy.name);
+console.log(cathy["name"]);
+
+cathy.city = "Seoul";
+//{name: 'cathy', age: 19, skills: ['자바스크립트', '파이썬', '코볼'], city: 'Seoul'}
+console.log(cathy);
+
+delete cathy.city;
+//{name: 'cathy', age: 19, skills: ['자바스크립트', '파이썬', '코볼']}
+console.log(cathy);
