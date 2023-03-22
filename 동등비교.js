3d055feb5c29a5217afecae40fd67b542b1273e8
leehyeryeong <w2108@e-mirim.hs.kr>
2023-03-22 09:05:51 +0900

    비교 연산자 ==, ===

---
+++ 동등비교.js
@@ -0,0 +1,11 @@
+//동등연산자(abstract equility)
+console.log(1 == '1');
+
+//일치연산자(strict equility) 자료형까지 비교
+console.log(1 === '1');
+
+console.log('hello' == 'hello');    //true
+console.log('hello' === 'hello');   //true
+
+console.log(0 == false);            //true
+console.log(0 === false);           //false
