49e08e1700814c0cc13a28f281936bc6b54fd333
leehyeryeong <w2108@e-mirim.hs.kr>
2023-04-05 23:54:50 +0900

    사진 관리 초기 설정 및 fs.readdirSync

--- main.js
+++ main.js
@@ -3,8 +3,47 @@ const fs = require('fs');
 const url = require('url');
 
 const app = http.createServer(function(req, res){
-  let _url = req.url;
+  let _url = req.url; //문자열 형태
+  //문자열 형태의 _url을 object 형태로 변환
   let queryData = url.parse(_url, true).query;
+  let pathname = url.parse(_url, true).pathname;
+
+  if(pathname === '/') {
+    if(queryData == undefined) {
+      res.writeHead(200);
+      res.end("undefined");
+    } else {
+      //description: readFile을 통해 읽어온 실제 내용
+      fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description) {
+        const title = queryData.id;
+        const template = `
+          <!DOCTYPE html>
+          <html lang="en">
+            <head>
+              <meta charset="UTF-8" />
+              <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <title>Document</title>
+            </head>
+            <body>
+              <h1>Web1 - ${title}</h1>
+              <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/id?=CSS">CSS</a></li>
+                <li><a href="/id?=JavaScript">JavaScript</a></li>
+              </ul>
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+          </html>
+        `;
+
+        res.writeHead(200);
+        res.end(template);
+      });
+    }
+  }
+
   res.end(queryData.id);
 });
 
---
+++ 사진 관리/main.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+//dirPath에 있는 모든 파일 목록의 배열(하위경로까지)
+function getAllFiles(dirPath, arrOfFiles) {
+  //dirPath에 있는 모든 파일 목록의 배열(하위경로는 불가)
+  const files = fs.readdirSync(dirPath);
+
+  files.forEach(function(file) {
+    const destPath = dirPath + "\\" + file;
+    
+    //폴더(디렉토리)라면 해당 폴더에 있는 모든 파일 목록들을 체크한다
+    if(fs.statSync(destPath).isDirectory()) {
+      getAllFiles(destPath, arrOfFiles);
+    } else {
+    //파일이라면 파일 목록에 추가
+      arrOfFiles.push(destPath);
+    }
+  });
+
+  return arrOfFiles;
+}
+
+const files = getAllFiles(__dirname + "\\base", []);
+console.log(files.join('\n'));
