55444ef9ffb5d8de879680232bfb94a96d3353ed
leehyeryeong <w2108@e-mirim.hs.kr>
2023-04-19 10:06:19 +0900

    파일 복사, 삭제, 이름 추출

--- 문법/fs.js
+++ 문법/fs.js
@@ -7,5 +7,11 @@ const files = fs.readdirSync(__dirname);
 result = path.join(__dirname, files[i]);
 console.log(result);
 
+console.log(path.basename(result));   // path.basename() 파일명만 추출
+
+const copiedFile = path.join(__dirname, 'backtick.js')
+fs.copyFileSync(result, copiedFile);  // 파일 복사
+fs.unlinkSync(copiedFile);            // 파일 삭제
+
 //해당 파일에 대한 정보를 알 수 있음
 console.log(fs.stateSync(result));
