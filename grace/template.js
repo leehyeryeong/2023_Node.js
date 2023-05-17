dfed519189360364829555c3a6bc60b2bd005c6f
leehyeryeong <w2108@e-mirim.hs.kr>
2023-05-17 10:07:43 +0900

    renderFile을 프로미스 방식으로

--- grace/template.js
+++ grace/template.js
@@ -14,14 +14,10 @@ const server = http.createServer(function(req, res) {
 
   res.setHeader('Content-Type', 'text/html');
   if(req.url === '/') {
-    //스트리밍 방식으로 index의 html 코드들을 전송(대용량 처리에 유리)
-    //파일 입출력: 파일을 한꺼번에 처리함
-    //스트림: 파일을 부분적으로 쪼개서 실시간으로 처리함
+    //renderFile(ejs 대상 경로, ejs에 넘겨줄 데이터)
     ejs.renderFile(path.join(__dirname, 'template', 'index.ejs'), 
-    {name: index.name, here: index.here}, 
-    function(err, data) {
-      res.end(data);
-    });
+    {name: index.name, here: index.here})
+    .then((data) => res.end(data));
   }
   else if(req.url === '/food') {
     fs.createReadStream(path.join(__dirname, 'html', 'food.html')).pipe(res);
